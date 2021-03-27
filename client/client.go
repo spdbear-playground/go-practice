489a357b4bbd012dee9d00af72a9ee5483d74071
spdbear <16793732+spdbear@users.noreply.github.com>
2021-03-28 02:16:50 +0900

    bodyを取れるようにした

--- client/client.go
+++ client/client.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 )
 
 func main() {
-	resp, err := http.Get("http://example.com/")
-	fmt.Printf("%v\n%v", resp, err)
+	resp, err := http.Get("http://localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	fmt.Printf("%s\n%v", body, err)
 }
